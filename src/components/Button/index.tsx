649e43356eede81ae7278a3c2e60124cca72f41c
Icaro Oliveira <icarovinici@gmail.com>
2021-02-01 19:50:45 -0300

    feat: add button component with tests

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import * as Styled from './styles'
+
+export type ButtonProps = {
+  children?: React.ReactNode
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: JSX.Element
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth = false,
+  icon,
+  ...props
+}: ButtonProps) => (
+  <Styled.Container
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    {...props}
+  >
+    {!!icon && icon}
+    {!!children && <span>{children}</span>}
+  </Styled.Container>
+)
+
+export default Button
