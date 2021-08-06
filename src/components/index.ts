3ecf50a5da47d62aa6ee79510ff94d3cbeee4492
Icaro Oliveira <icarovinici@gmail.com>
2021-08-05 21:14:08 -0300

    feat: create a unique file to export components and types

---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+export { default as Banner } from 'components/Banner'
+export { default as BannerSlider } from 'components/BannerSlider'
+
+export { default as Button } from 'components/Button'
+
+export { Container } from 'components/Container'
+
+export { default as Footer } from 'components/Footer'
+
+export { default as GameCard } from 'components/GameCard'
+export { default as GameCardSlider } from 'components/GameCardSlider'
+
+export { default as Heading } from 'components/Heading'
+export { default as LineColors } from 'components/Heading'
+
+export { default as Highlight } from 'components/Highlight'
+
+export { default as Logo } from 'components/Logo'
+
+export { default as Menu } from 'components/Menu'
+
+export { default as Ribbon } from 'components/Ribbon'
+
+export { default as Slider } from 'components/Slider'
---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type { BannerProps } from 'components/Banner'
+export type { BannerSliderProps } from 'components/BannerSlider'
+
+export type { ButtonProps } from 'components/Button'
+
+export type { GameCardProps } from 'components/GameCard'
+export type { GameCardSliderProps } from 'components/GameCardSlider'
+
+export type { HeadingProps } from 'components/Heading'
+export type { LineColors } from 'components/Heading'
+
+export type { HighlightProps } from 'components/Highlight'
+
+export type { LogoProps } from 'components/Logo'
+
+export type { MediaMatchProps } from 'components/Mediamatch'
+
+export type { MenuProps } from 'components/Menu'
+
+export type { RibbonProps } from 'components/Ribbon'
+
+export type { SliderProps } from 'components/Slider'
